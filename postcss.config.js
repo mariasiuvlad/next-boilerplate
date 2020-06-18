137e68715a412a0a7178a94408196b761a2fde1c
Mariasiu Vlad <mariasiuvlad@gmail.com>
2020-06-18 18:36:24 +0300

    Add storybook (#7)
    - add storybook
    - fix storybook compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
+// nextjs wants deps without require while storybook wants them with require
+const withStorybook = (dep) => (process.env.IS_STORYBOOK ? require(dep) : dep)
+
+const plugins = ['tailwindcss', 'postcss-preset-env'].map(withStorybook)
+
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-preset-env'],
+  plugins,
 }
