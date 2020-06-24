22daac176f597632254917dc8ef056243d593270
Vlad Mariasiu <mariasiuvlad@gmail.com>
2020-06-24 18:30:43 +0300

    Organize auth context types

--- src/context/auth/types.ts
+++ src/context/auth/types.ts
@@ -1,3 +1,6 @@
+import {TJWTToken} from '@lib/api/auth/types'
+import {AxiosError} from 'axios'
+
 export enum Action {
   Logout = 'Logout',
   LoginStart = 'Login/Start',
@@ -5,4 +8,41 @@ export enum Action {
   LoginError = 'Login/Error',
   RefreshStart = 'Refresh/Start',
   RefreshError = 'Refresh/Error',
+}
+
+export namespace ActionTypes {
+  export const Logout = 'Logout'
+  export const LoginStart = 'Login/Start'
+  export const LoginSuccess = 'Login/Success'
+  export const LoginError = 'Login/Error'
+  export const RefreshStart = 'Refresh/Start'
+  export const RefreshError = 'Refresh/Error'
+}
+
+export namespace Actions {
+  export interface ILogout {
+    type: typeof ActionTypes.Logout
+  }
+  export interface ILoginStart {
+    type: typeof ActionTypes.LoginStart
+  }
+  export interface ILoginSuccess {
+    type: typeof ActionTypes.LoginSuccess
+    payload: TJWTToken
+  }
+  export interface ILoginError {
+    type: typeof ActionTypes.LoginError
+    payload: AxiosError
+  }
+  export interface IRefreshStart {
+    type: typeof ActionTypes.RefreshStart
+  }
+  export interface IRefreshSuccess {
+    type: typeof ActionTypes.LoginSuccess
+    payload: TJWTToken
+  }
+  export interface IRefreshError {
+    type: typeof ActionTypes.RefreshError
+    payload: AxiosError
+  }
 }
