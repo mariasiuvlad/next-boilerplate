340f49b22c8a4ae4d405796dada4b79749d3b49f
Vlad Mariasiu <mariasiuvlad@gmail.com>
2020-06-24 17:58:19 +0300

    Added initial local dev infrastructure

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,10 +1,14 @@
+import AvatarDropzone from '@components/util/AvatarDropzone'
+
 const User = ({user}) => (
   <div className="text-sm p-8 bg-white border border-gray-300 rounded-lg">
-    <img
-      className="w-10 h-10 rounded-full mb-4"
-      src={user.avatar_url}
-      alt={'Avatar of ' + user.display_name}
-    />
+    <AvatarDropzone>
+      <img
+        className="w-10 h-10 rounded-full mb-4"
+        src={user.avatar_url}
+        alt={'Avatar of ' + user.display_name}
+      />
+    </AvatarDropzone>
     <p className="text-gray-900 leading-none">{user.display_name}</p>
   </div>
 )
