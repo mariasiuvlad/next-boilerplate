8ce294557b83b94a0e395247e62d87abdbaec81d
Vlad Mariasiu <mariasiuvlad@gmail.com>
2020-06-27 08:24:32 +0300

    test react-hooks

---
+++ src/lib/hooks/useAuth.test.tsx
@@ -0,0 +1,14 @@
+import {renderHook, act} from '@testing-library/react-hooks'
+import {useAuth} from './useAuth'
+import {MockedProvider} from '@apollo/react-testing'
+
+const wrapper = ({children}) => <MockedProvider>{children}</MockedProvider>
+
+test('should increment counter', async () => {
+  const {result} = renderHook(() => useAuth(), {wrapper})
+
+  // test initial state
+  expect(result.current.initialized).toBe(false)
+  expect(result.current.loading).toBe(false)
+  expect(result.current.data).toBe(null)
+})
