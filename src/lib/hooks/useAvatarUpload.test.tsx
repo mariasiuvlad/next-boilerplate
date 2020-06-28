304ca3f54f195a207aa86a22f882d365ad45f03f
Vlad Mariasiu <mariasiuvlad@gmail.com>
2020-06-28 16:04:26 +0300

    Fix useAvatarUpload test errors

--- src/lib/hooks/useAvatarUpload.test.tsx
+++ src/lib/hooks/useAvatarUpload.test.tsx
@@ -20,20 +20,22 @@ test('should correctly handle file upload', async () => {
     return Promise.resolve({request: {responseURL: '<Avatar URL>'}})
   })
 
-  const {result} = renderHook(() => useAvatarUpload(), {wrapper})
+  const {result, waitForNextUpdate} = renderHook(() => useAvatarUpload(), {
+    wrapper,
+  })
   // initial state
   expect(result.current.error).toBe(undefined)
   expect(result.current.loading).toBe(false)
   expect(result.current.progress).toBe(0)
 
-  const promise = result.current.upload(null)
-
-  await act(async () => {
-    // loading state
-    expect(result.current.loading).toBe(true)
-    expect(result.current.progress).toBe(20)
-    await promise
+  act(() => {
+    result.current.upload(null)
   })
+
+  expect(result.current.loading).toBe(true)
+  expect(result.current.progress).toBe(20)
+
+  await waitForNextUpdate()
 
   // state resets after succesful upload
   expect(result.current.error).toBe(undefined)
@@ -47,15 +49,16 @@ test('should correctly handle errors', async () => {
     return Promise.reject(new Error('Test Error'))
   })
 
-  const {result} = renderHook(() => useAvatarUpload(), {wrapper})
-  const promise = result.current.upload(null)
-
-  await act(async () => {
-    expect(result.current.loading).toBe(true)
-    expect(result.current.progress).toBe(20)
-    await promise
+  const {result, waitForNextUpdate} = renderHook(() => useAvatarUpload(), {
+    wrapper,
   })
+  act(() => {
+    result.current.upload(null)
+  })
+  expect(result.current.loading).toBe(true)
+  expect(result.current.progress).toBe(20)
 
+  await waitForNextUpdate()
   expect(result.current.error.message).toEqual('Test Error')
   expect(result.current.loading).toBe(false)
   expect(result.current.progress).toBe(0)
