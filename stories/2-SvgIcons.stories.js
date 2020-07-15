d855e4412fb2cea177daedc488196d9c572ad1a4
Mariasiu Vlad <mariasiuvlad@gmail.com>
2020-07-15 09:10:52 +0300

    Add Playtwin SVG icons (#37)

--- stories/2-SvgIcons.stories.js
+++ stories/2-SvgIcons.stories.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import '../src/styles/index.css'
 import CottonCandyIcon from '@components/Icons/cotton-candy.svg'
+import AlienIcon from '@components/Icons/ptw_alien.svg'
+import CoinIcon from '@components/Icons/ptw_home.svg'
+import NotificationIcon from '@components/Icons/ptw_notification.svg'
+import RocketIcon from '@components/Icons/ptw_rocket.svg'
 
 export default {
   title: 'SVG Icons',
@@ -15,5 +19,14 @@ export const SmallSVGIcon = () => (
 export const MediumSVGIcon = () => (
   <div className="bg-pink-400 rounded-full inline-block p-8 border border-gray-300">
     <CottonCandyIcon className="h-16 w-16" />
+  </div>
+)
+
+export const PlaytwinIcons = () => (
+  <div className="flex bg-pink-400 rounded-full p-8 border border-gray-300">
+    <AlienIcon className="h-8 w-8" />
+    <CoinIcon className="h-8 w-8" />
+    <NotificationIcon className="h-8 w-8" />
+    <RocketIcon className="h-8 w-8" />
   </div>
 )
