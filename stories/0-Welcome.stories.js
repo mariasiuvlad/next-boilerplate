7fe17e7d8e86e448d05d342398d20b275436d5ec
Mariasiu Vlad <mariasiuvlad@gmail.com>
2020-07-17 19:11:17 +0300

    Fix storybook (#44)
    * Fix storybook global import
    * Add `yarn build` and `yarn storybook:build` to pr test
    * Cleanup Storybook

--- stories/0-Welcome.stories.js
+++ stories/0-Welcome.stories.js
@@ -7,8 +7,20 @@ export default {
   component: Welcome,
 }
 
-export const ToStorybook = () => <Welcome showApp={linkTo('Button Example')} />
+export const ToStorybook = () => (
+  <div>
+    <p1 className="text-2xl">Welcome to PlayTwin</p1>
+    <ul className="mt-4">
+      <li>
+        <button onClick={linkTo('Button Example')}>Buttons</button>
+      </li>
+      <li>
+        <button onClick={linkTo('Icons')}>Icons</button>
+      </li>
+    </ul>
+  </div>
+)
 
 ToStorybook.story = {
-  name: 'to Storybook',
+  name: 'Index',
 }
--- stories/1-SvgIcons.stories.js
+++ stories/1-SvgIcons.stories.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../src/styles/index.css'
 import CottonCandyIcon from '@components/Icons/cotton-candy.svg'
 import AlienIcon from '@components/Icons/ptw_alien.svg'
 import CoinIcon from '@components/Icons/ptw_home.svg'
@@ -7,26 +6,26 @@ import NotificationIcon from '@components/Icons/ptw_notification.svg'
 import RocketIcon from '@components/Icons/ptw_rocket.svg'
 
 export default {
-  title: 'SVG Icons',
+  title: 'Icons',
 }
 
 export const SmallSVGIcon = () => (
-  <div className="bg-pink-400 rounded-full inline-block p-4 border border-gray-300">
+  <div className="bg-yellow-500 rounded-full inline-block p-4 border border-gray-300">
     <CottonCandyIcon className="h-8 w-8" />
   </div>
 )
 
 export const MediumSVGIcon = () => (
-  <div className="bg-pink-400 rounded-full inline-block p-8 border border-gray-300">
+  <div className="bg-yellow-500 rounded-full inline-block p-8 border border-gray-300">
     <CottonCandyIcon className="h-16 w-16" />
   </div>
 )
 
 export const PlaytwinIcons = () => (
-  <div className="flex bg-pink-400 rounded-full p-8 border border-gray-300">
-    <AlienIcon className="h-8 w-8" />
-    <CoinIcon className="h-8 w-8" />
-    <NotificationIcon className="h-8 w-8" />
-    <RocketIcon className="h-8 w-8" />
+  <div className="flex bg-yellow-500 rounded-full p-8 border border-gray-300">
+    <AlienIcon className="h-8 w-8 mx-2" />
+    <CoinIcon className="h-8 w-8 mx-2" />
+    <NotificationIcon className="h-8 w-8 mx-2" />
+    <RocketIcon className="h-8 w-8 mx-2" />
   </div>
 )
